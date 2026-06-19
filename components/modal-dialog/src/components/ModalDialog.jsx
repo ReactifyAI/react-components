dff73f4fcfee1ce39e6362213f9a46ead8de224f
Shraddha Punjabi <punjabishraddha19@gmail.com>
2026-06-18 20:55:51 -0700

    implemented required ARIA roles and properties

--- components/modal-dialog/src/components/ModalDialog.jsx
+++ components/modal-dialog/src/components/ModalDialog.jsx
@@ -3,15 +3,33 @@ import './ModalDialog.css'
 export default function ModalDialog({ isOpen, onClose, title, children }) {
   if(!isOpen) return null
 
+  const titleId = "modal-title"
+  const descriptionId = "modal-description"
+
   return (
     <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-container" onClick={(e) => e.stopPropagation()}>
+      <div
+        className="modal-container"
+        onClick={(e) => e.stopPropagation()}
+        role="dialog"
+        aria-modal={true}
+        aria-labelledby={titleId}
+        aria-describedby={descriptionId}
+      >
         <div className="title-container">
-          <h2 className="modal-title">{title}</h2>
-          <button className="modal-close-button" onClick={onClose}>X</button>
+          <h2 id={titleId} className="modal-title">
+            {title}
+          </h2>
+          <button
+            className="modal-close-button"
+            onClick={onClose}
+            aria-label="Close dialog"
+          >
+            X
+          </button>
         </div>
 
-        <div className="modal-content">{children}</div>
+        <div id={descriptionId} className="modal-content">{children}</div>
       </div>
     </div>
   )
